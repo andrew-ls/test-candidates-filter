6369b8ab15877895a3c01474f6fab914157b135b
Andrew Steel <dev@andrewsteel.net>
2019-05-19 19:11:27 +0100

    Splits out CSS and JavaScript from `public/index.html` into separate `public/styles.css` and `public/scripts.js` files.

---
+++ public/scripts.js
@@ -0,0 +1,69 @@
+/*
+ * Modifications copyright (c) 2019 Andrew Steel <copyright@andrewsteel.net>
+*/
+
+const newCandidates = [
+	{ name: "Kerrie", skills: ["JavaScript", "Docker", "Ruby"] },
+	{ name: "Mario", skills: ["Python", "AWS"] },
+	{ name: "Jacquline", skills: ["JavaScript", "Azure"] },
+	{ name: "Kathy", skills: ["JavaScript", "Java"] },
+	{ name: "Anna", skills: ["JavaScript", "AWS"] },
+	{ name: "Matt", skills: ["PHP", "AWS"] },
+	{ name: "Matt", skills: ["PHP", ".Net", "Docker"] },
+];
+
+function removeRowsFromTable (table)
+{
+	const rows = table.getElementsByTagName("tr");
+
+	while (rows.length > 1) {
+		table.deleteRow(1);
+	}
+}
+
+function insertCandidate (tbody, name, skills)
+{
+	const newRow = tbody.insertRow();
+	const nameCell = newRow.insertCell();
+	const skillCell = newRow.insertCell();
+
+	const candidateName = document.createTextNode(name);
+	const candidateSkills = document.createTextNode(skills.join(", "));
+
+	nameCell.appendChild(candidateName);
+	skillCell.appendChild(candidateSkills);
+}
+
+function addCandidatesToTable (table, candidates)
+{
+	candidates.forEach((candidate) => (
+		insertCandidate(table, candidate.name, candidate.skills)
+	));
+}
+
+function filterCandidateBySkill (candidates, skill)
+{
+	/*
+	 * Assuming the use of an ES6 arrow function in addCandidatesToTable means
+	 * I can use ES6 array spread syntax here.
+	*/
+	return candidates.reduce((matchingCandidates, candidate) => (
+		candidate.skills.includes(skill)
+			? [...matchingCandidates, candidate]
+			: matchingCandidates
+	), []);
+}
+
+const candidatesTable = document.getElementById("candidates_example");
+const newCandidatesTable = candidatesTable.cloneNode(true);
+
+removeRowsFromTable(newCandidatesTable);
+const newTbody = newCandidatesTable.getElementsByTagName("tbody")[0];
+
+const filteredCandidates = filterCandidateBySkill(
+	newCandidates,
+	"JavaScript"
+);
+addCandidatesToTable(newTbody, filteredCandidates);
+
+document.body.appendChild(newCandidatesTable);
